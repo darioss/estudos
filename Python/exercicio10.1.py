9c035f1b9d878a5b7a8393ca7bcf753de4d2d15d
Dario Souza da Silva <dariosouzadasilva@gmail.com>
2020-10-08 03:17:25 -0300

    Conclusão de exercícios

--- Python/exercicio10.1.py
+++ Python/exercicio10.1.py
@@ -52,20 +52,100 @@ def indice(item, lista):
     indice da posicao em que item ocorre na lista.
     Caso item nao ocorra na lista a funcao retorna None
     '''
-    if item in lista:
-        return lista.index(item)
-    else:
-        return "item não pertence a lista"
+    parametro = str(item)
+    aux = []
+    for i in range(len(lista)):
+        aux.append(str(lista[i]))
 
+    if parametro in aux:
+        return aux.index(parametro)
+    else:
+        return "item não pertence à lista"
+
+def contaOcorrencias(item, lista):
+    '''
+    Recebe um item e uma lista e conta quantas vezes o item
+    aparece na lista.
+    '''
+    if item in lista:
+        return lista.count(item)
+    else:
+        return "item não petence à lista"
+
+def soma_elementos(inicio, fim, lista):
+    '''
+    Recebe uma lista e dois parametros de início e fim
+    para a montagem de uma sub-lista a partir da lista
+    original. Retorna a soma dos valores de cada indice
+    da sub-lista.
+    '''
+    sublista = lista[inicio:fim]
+    return sum(sublista)
 
 # testes
-lista  = [1, "oi", 3.14, 7, True]
-print(geraLista(5, 8, False))
+lista  = [1, "oi", 3.14, 7, True, "oi", "oi"]
+lista2  = [1, 2, 3, 4, 5]
 
-print(pertence("oi", lista))
+print("######################################################")
+print("######  -  TESTA A FUNÇÃO CONTA OCORRÊNCIAS  -  ######")
+print("######################################################")
+# teste 1
+if contaOcorrencias("oi", lista) == 3:
+    print("Passou no primeiro teste :-)")
+else:
+    print("Nao passou no primeiro teste! :-(")
 
-print("O índice é", indice(3.14, lista))
+if contaOcorrencias(True, lista):
+    print("Passou no Segundo teste :-)")
+else:
+    print("Nao passou no segundo teste! :-(")
 
+print("###########################################")
+print("######  -  TESTA A FUNÇÃO ÍNDICE  -  ######")
+print("###########################################")
+# teste 1
+if indice(3.14, lista) == 2:
+    print("Passou no primeiro teste :-)")
+else:
+    print("Nao passou no primeiro teste! :-(")
+
+
+if indice(7, lista) == 3:
+    print("Passou no segundo teste :-)")
+else:
+    print("Nao passou no segundo teste! :-(")
+
+if indice(True, lista) == 4:
+    print("Passou no terceiro teste :-)")
+else:
+    print("Nao passou no terceiro teste! :-(")
+
+if indice(718, lista) == 12:
+    print("Passou no Quarto teste :-)")
+else:
+    print("Nao passou no quarto teste! :-(")
+
+
+print("###############################################")
+print("######  -  TESTA A FUNÇÃO GERA LISTA  -  ######")
+print("###############################################")
+# teste 1
+if geraLista(5, 8, False):
+    print("Gerou lista aleatória não ordenada :-)")
+    print(geraLista(5, 8, False))
+else:
+    print("Nao passou no primeiro teste! :-(")
+
+# teste 2
+if geraLista(5, 8, True):
+    print("Gerou lista aleatória ordenada :-)")
+    print(geraLista(5, 8, True))
+else:
+    print("Nao passou no primeiro teste! :-(")
+
+print("#############################################")
+print("######  -  TESTA A FUNÇÃO PERTENCE  -  ######")
+print("#############################################")
 # teste 1
 if pertence("oi",lista):
     print("Passou no primeiro teste! :-)")
@@ -89,3 +169,24 @@ if pertence(False,lista):
     print("Passou no quarto teste! :-)")
 else:
     print("Não passou no quarto teste! :-(")
+
+print("#############################################")
+print("####  - TESTA A FUNÇÃO SOMA ELEMENTOS -  ####")
+print("#############################################")
+# teste 1
+if soma_elementos(0,len(lista2),lista2) == 15:
+    print("Passou no primeiro teste! :-)")
+else:
+    print("Nao passou no primeiro teste! :-(")
+
+# teste 2
+if soma_elementos(1,4,lista2) == 9:
+    print("Passou no segundo teste! :-)")
+else:
+    print("Nao passou no segundo teste! :-(")
+
+# teste 3
+if soma_elementos(2,2,lista2) == 0:
+    print("Passou no terceiro teste! :-)")
+else:
+    print("Nao passou no terceiro teste! :-(")
