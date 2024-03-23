289903bd74931f5d34195e3b5e476721b72ae975
Dario Silva <dariosouzadasilva@gmail.com>
2024-03-23 15:48:57 -0300

    Input output tests with conditions

--- Python/DIO/input_output.py
+++ Python/DIO/input_output.py
@@ -1,3 +1,14 @@
 name = input("What is your name?\n")
 age = input("What is your age?\n")
 print(name, age, sep=" - ", end=" ...")
+
+SERVICES = ["Administrative","Commercial","Support"]
+select_service = input(f"Select 1 for {SERVICES[0]}, 2 for {SERVICES[1]} or 3 for {SERVICES[2]}\n")
+
+if (int(select_service) == 1):
+  print(f"{SERVICES[0]} is selected")
+elif(int(select_service) == 2):
+  print(f"{SERVICES[1]} is selected")
+else:
+  print(f"{SERVICES[2]} is selected")
+
