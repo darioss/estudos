309c2428e42b93c802f08f4eeb09aa6c81f2e5db
Dario Silva <dariosouzadasilva@gmail.com>
2024-10-02 09:50:41 -0300

    Novos exercícios
    Trilha C# Microsoft Learn

---
+++ CSharp/TestProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = {"Alex", "Eddie", "David", "Michael"};
+            
+            foreach(var name in names)
+            {
+                Console.WriteLine(name);
+            }
+            
+            for( int i = names.Length - 1; i >=0; i--)
+            {
+                if(names[i]=="David") names[i] ="Sammy";
+            }
+
+            Console.WriteLine("-------------------------------------");
+
+            foreach(var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
